d914ebb8d1eb367fa8a463f7fdf32e6d8df167ef
273* <keita.y310@icloud.com>
2025-08-06 18:21:41 +0900

    親のidにひも付いたメッセージ作成処理を実装

--- src/TreeStructure.tsx
+++ src/TreeStructure.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import type { MessageObj } from "../utils/chatTree/type";
 
 const TreeStructure = () => {
@@ -7,23 +8,30 @@ const TreeStructure = () => {
   const messageRef = useRef<HTMLInputElement>(null);
   const roleRef = useRef<HTMLInputElement>(null);
 
-  // 送信ボタンの挙動
-  const handleClick = async () => {
-    // 入力状態
+  // メッセージ作成の共通関数
+  const createMessage = (parentId?: string) => {
     const inputMessage = messageRef?.current?.value;
     const inputRole = roleRef?.current?.checked;
 
-    // 更新処理
     if (inputMessage) {
       const newMessage: MessageObj = {
-        id: "hoge",
+        id: uuidv4(),
         message: inputMessage,
         role: inputRole ? "user" : "assistant",
         createdAt: new Date(),
+        ...(parentId ? { parentId } : {}),
       };
 
       setMessages((prevMessages) => [...prevMessages, newMessage]);
     }
+  };
+
+  const handleCreateMessage = () => {
+    createMessage();
+  };
+
+  const handleCreateBranch = (parentId: string) => {
+    createMessage(parentId);
   };
 
   return (
@@ -33,7 +41,7 @@ const TreeStructure = () => {
         <input ref={messageRef} type="text" placeholder="Enter text" />
         <label htmlFor="role">user?</label>
         <input ref={roleRef} type="checkbox" id="role" />
-        <button type="submit" onClick={handleClick}>
+        <button type="submit" onClick={handleCreateMessage}>
           Submit
         </button>
       </div>
@@ -45,9 +53,13 @@ const TreeStructure = () => {
               <p>{message.role}</p>
               <p>{message.createdAt.getTime()}</p>
               <h3>{message.message}</h3>
+              <div>
+                <p>{message.id}</p>
+                {message.parentId && <p>Parent ID: {message.parentId}</p>}
+              </div>
               <button
                 type="submit"
-                onClick={() => console.log("created branch!")}
+                onClick={() => handleCreateBranch(message.id)}
               >
                 create branch
               </button>
